a88ce13fbc2f62394775fb5fcd4f0207dbe3e409
Андрій Справка <113443611+s1reeen@users.noreply.github.com>
2024-11-06 20:48:14 +0200

    initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+  return (
+    <div>
+        <h1>Redux Todolist</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+export default Header
